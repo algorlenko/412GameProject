33bfb3bcad98ef4d8cb9c30f609e66a91ebf7179
Sasha <algorlenko@gmail.com>
2019-07-13 19:14:16 -0400

    This is the very second version 0.1

--- src/ShopKeeper.java
+++ src/ShopKeeper.java
@@ -12,7 +12,7 @@ GameEngine myEngine;
         myLayer = 3;
 myEngine = passedEngine;
 
-        unitImage = "/Enemigos/merchant_a_shopKeeper.png";
+        unitImage = "/Enemigos/merchant_a_shopkeeper.png";
         image = generateImage(unitImage);
         x = myX;
         y = myY;
